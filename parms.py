93f4b8e1fe24abe6227308bdc75c32d5618243ca
Henn363 <henn363@gmail.com>
2020-04-27 10:30:13 -0400

    Create parms.py

---
+++ parms.py
@@ -0,0 +1,12 @@
+class userVals():
+    key = '4d1a0a025fb88ddb0c8ae76be3e71623-3df42e41ae2cf03957285b21e3eb9ac4'
+    accountID = '101-001-14057689-001'
+    risk= 0.02
+    pair= "EUR_USD"
+    tradeTime = 60
+    ticks = 10
+    count = 10
+    params = {
+    	"count": count,
+    	"granularity": "D"
+    }
